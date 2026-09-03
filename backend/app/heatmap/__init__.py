07c5fa7f28e3f8566daed1929da5b84d024c6703
Mansi Chavande <cmansi2310@gmail.com>
2026-09-03 19:24:19 +0530

    Add Supabase backend connectivity

---
+++ backend/app/heatmap/__init__.py
@@ -0,0 +1,9 @@
+from .clustering import perform_geographical_clustering
+from .scoring import calculate_cluster_severity
+from .processor import process_heatmap_pipeline
+
+__all__ = [
+    'perform_geographical_clustering',
+    'calculate_cluster_severity',
+    'process_heatmap_pipeline'
+]
